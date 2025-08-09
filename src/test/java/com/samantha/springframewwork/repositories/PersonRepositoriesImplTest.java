f6f11e7b12dd579ad5d6106f680481a8f8dc716b
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-08-09 19:52:18 +0100

    Created test for FLux Operations

--- src/test/java/com/samantha/springframewwork/repositories/PersonRepositoriesImplTest.java
+++ src/test/java/com/samantha/springframewwork/repositories/PersonRepositoriesImplTest.java
@@ -2,7 +2,10 @@ package com.samantha.springframewwork.repositories;
 
 import com.samantha.springframewwork.domain.Person;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,10 +39,35 @@ class PersonRepositoriesImplTest {
     void testMapOperation() {
         Mono<Person> personMono = personRepository.getById(3);
 
-        personMono.map((person -> {
-            return person.getLastName();
-        })).subscribe(lastName -> {
-            System.out.println(lastName);
+        personMono.map((Person::getLastName)).subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+                personFlux.map(Person::getFirstName)
+                .subscribe(System.out::println);
+        // This is a blocking operation, not recommended in reactive programming
+        Person person = personFlux.blockFirst();
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> listMono = personFlux.collectList();
+        listMono.subscribe(list -> {
+            list.forEach(person -> System.out.println(person.getFirstName()));
         });
     }
 }
